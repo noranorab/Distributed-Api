f4b5dbab8119403ae95fc698d0c7a674d1c24aaa
noranorab <noranorab7@gmail.com>
2024-03-27 23:22:52 +0000

    enabled scheduling

--- src/main/java/com/missingsemester/hashmapapi/HashmapApiApplication.java
+++ src/main/java/com/missingsemester/hashmapapi/HashmapApiApplication.java
@@ -5,10 +5,12 @@ import com.missingsemester.hashmapapi.api.service.ShardMonitor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @Import(AppConfig.class)
+@EnableScheduling
 public class HashmapApiApplication {
 
     public static void main(String[] args) {
